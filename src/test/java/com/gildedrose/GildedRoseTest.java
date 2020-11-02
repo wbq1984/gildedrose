c994a66d41b86b4592011f4c860ef29d9101834d
wbq1984 <wbqreg@yeah.net>
2020-11-02 19:38:28 +0800

    file reading

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,23 @@
 package com.gildedrose;
 
+import com.gildedrose.util.ReadFile;
+import org.assertj.core.api.Assert;
+import org.junit.Test;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 class GildedRoseTest {
+    @Test
+    public void should_always_align_wiht_baseline() throws Exception {
+        String output = TextTestFixture.getBaseline();
+        String fileTest = ReadFile.getFile("src/test/baseline.txt");
+        System.out.println("_-----"+fileTest);
+        assertThat(output).isEqualTo(fileTest);
 
+
+    }
 
 
 }
