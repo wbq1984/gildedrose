e380e327a2508530a4cfcbafc45f36848fb5dc6b
wbq1984 <wbqreg@yeah.net>
2020-11-03 22:15:53 +0800

    google files method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,28 +1,25 @@
 package com.gildedrose;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
-import com.gildedrose.util.ReadFile;
-import org.assertj.core.api.Assert;
 import org.junit.Test;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class GildedRoseTest {
 
     @Test
     public void should_always_align_wiht_baseline() throws Exception {
-        String output = TextTestFixture.getBaseline().replaceAll("\r\n","");
-//        String baseline = ReadFile.getFile("src/test/baseline.txt");
-        File file = new File("src/test/baseline.txt");
-//        List<String> lines = Files.readLines(file, Charsets.UTF_8);
-        String baseline = Files.asCharSource(file, Charsets.UTF_8).read();
+        String output = TextTestFixture.getBaseline().replaceAll("\r\n", "");
+        /*
+            生成baseline.txt
+            File baselineFile = new File("src/test/baseline.txt");
+            String baseline = Files.asCharSource(baselineFile, Charsets.UTF_8).read();
+            Files.asCharSink(baselineFile, Charsets.UTF_8).write(output);
+         */
+        //读取生成baseline.txt 文件内容
+        String baseline = Files.asCharSource(new File("src/test/baseline.txt"), Charsets.UTF_8).read();
         assertThat(output).isEqualTo(baseline);
-
-
     }
 
 
