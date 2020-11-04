ee77a047172cdc3aa90f555357c75a06a2327454
wbq1984 <wbqreg@yeah.net>
2020-11-04 19:20:06 +0800

    inline refactor

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,8 +14,25 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    protected boolean isBackstagePass() {
+        /*
+            https://blog.csdn.net/qq_42499188/article/details/85193886 迪米特法则
+            return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+            item.name 违反了迪米特法则
+         */
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    protected boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    protected boolean isAgedbrie() {
+        return name.equals("Aged Brie");
     }
 }
